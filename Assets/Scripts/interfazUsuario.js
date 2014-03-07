7df693be22e1a990e8a59b86c87c00d878cb7386
jPalazon <jospalazon@gmail.com>
2014-03-07 18:23:06 +0000

    menu

--- Assets/Scripts/interfazUsuario.js
+++ Assets/Scripts/interfazUsuario.js
@@ -9,7 +9,7 @@ function Update () {
 }
 
 function OnGUI () {
-	GUI.Box(Rect(Screen.width-Screen.width/4, 0, Screen.width/4, Screen.height), "Menu");
+	GUI.Box(Rect(Screen.width-Screen.width/4, 0, Screen.width/4, Screen.height), "menu de prueba");
 	GUI.Label(Rect(Screen.width-Screen.width/4+100, 15, 200, 30), "pruebita");
 //	if (GUI.Button(Rect(Screen.width/2-50, Screen.height/2-15, 100, 30), "Mi boton")) {
 //		salud -= 10;
