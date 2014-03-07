7c833ce71123caa6bdd47c18d3127b9146aa8a8f
AdrianMBarrera <mbarreraadrian@gmail.com>
2014-03-07 16:50:20 +0000

    Comienzo de la interfaz de usuario.

---
+++ Assets/Scripts/interfazUsuario.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+	GUI.Box(Rect(Screen.width-Screen.width/4, 0, Screen.width/4, Screen.height), "Menu");
+	GUI.Label(Rect(Screen.width-Screen.width/4+100, 15, 200, 30), "SALUD");
+//	if (GUI.Button(Rect(Screen.width/2-50, Screen.height/2-15, 100, 30), "Mi boton")) {
+//		salud -= 10;
+//	}
+//	GUI.Label(Rect(Screen.width/2-100, Screen.height/2+20, 200, 30), "SALUD: " + salud);
+}
