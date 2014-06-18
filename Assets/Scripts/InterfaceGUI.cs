8b75ff548cfb5c63c3b6e5492c93f0a05deeed9c
AdrianMBarrera <mbarreraadrian@gmail.com>
2014-06-18 16:31:49 +0100

    Actualizacion con escena 2

---
+++ Assets/Scripts/InterfaceGUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceGUI : MonoBehaviour {
+
+	int toolbarInt = 0;
+	string[] toolbarStrings = new string[] {"Restrictions", "Start position", "Final position", "Save"};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		GUI.Box(new Rect(Screen.width-Screen.width/3, 0, Screen.width/3, Screen.height),"\nMOVEMENTS INTERFACE");
+
+		toolbarInt = GUI.Toolbar (new Rect (Screen.width - Screen.width/3 + 30, 50, 400, 25), toolbarInt, toolbarStrings);
+		
+//		switch (toolbarInt) {			
+//			case 0: //condRest = true;
+//					//step1();
+//					break;
+//				
+//			case 1: //condRest = false;
+//					//step2();
+//					break;
+//				
+//			case 2: //condRest = false;
+//					//step3();
+//					break;
+//				
+//			case 3: //condRest = false;
+//					//saveExercise();
+//					break;
+//				
+//			default: Debug.Log("Ninguna opcion valida.");
+//		}
+	}
+}
--- Assets/Scripts/cameraSelection.js
+++ Assets/Scripts/cameraSelection.js
@@ -5,9 +5,9 @@ var camt : Camera;
 var camd : Camera;
 var cami : Camera;
 var camc : Camera;
-var planoX : GameObject;
-var planoY : GameObject;
-var planoZ : GameObject;
+//var planoX : GameObject;
+//var planoY : GameObject;
+//var planoZ : GameObject;
 
 function Start () {
 	camf.enabled = true;
@@ -16,55 +16,54 @@ function Start () {
 	cami.enabled = false;
 	camc.enabled = false;
 	
-	planoX = GameObject.CreatePrimitive(PrimitiveType.Cube);
-	planoX.name = "PlanoX";
-	planoX.transform.localScale.x = 0;
-	planoX.transform.localScale.y = 8;
-	planoX.transform.localScale.z = 6;
-	planoX.transform.position.x = 0.5;
-	planoX.transform.position.y = 0;
-	planoX.transform.position.z = -0.1;
-	planoX.renderer.material.shader = Shader.Find("Transparent/Diffuse");
-	planoX.renderer.material.color = Color(0,1,0,0.2);
-	planoX.renderer.enabled=false;
+//	planoX = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//	planoX.name = "PlanoX";
+//	planoX.transform.localScale.x = 0;
+//	planoX.transform.localScale.y = 8;
+//	planoX.transform.localScale.z = 6;
+//	planoX.transform.position.x = 0.5;
+//	planoX.transform.position.y = 0;
+//	planoX.transform.position.z = -0.1;
+//	planoX.renderer.material.shader = Shader.Find("Transparent/Diffuse");
+//	planoX.renderer.material.color = Color(0,1,0,0.2);
+//	planoX.renderer.enabled=false;
+//	
+//	planoY = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//	planoY.name = "PlanoY";
+//	planoY.transform.localScale.x = 6;
+//	planoY.transform.localScale.y = 0;
+//	planoY.transform.localScale.z = 6;
+//	planoY.transform.position.x = 0.5;
+//	planoY.transform.position.y = 0;
+//	planoY.transform.position.z = -0.1;
+//	planoY.renderer.material.shader = Shader.Find("Transparent/Diffuse");
+//	planoY.renderer.material.color = Color(0,1,0,0.2);
+//	planoY.renderer.enabled=false;
 	
-	planoY = GameObject.CreatePrimitive(PrimitiveType.Cube);
-	planoY.name = "PlanoY";
-	planoY.transform.localScale.x = 6;
-	planoY.transform.localScale.y = 0;
-	planoY.transform.localScale.z = 6;
-	planoY.transform.position.x = 0.5;
-	planoY.transform.position.y = 0;
-	planoY.transform.position.z = -0.1;
-	planoY.renderer.material.shader = Shader.Find("Transparent/Diffuse");
-	planoY.renderer.material.color = Color(0,1,0,0.2);
-	planoY.renderer.enabled=false;
-	
-	planoZ = GameObject.CreatePrimitive(PrimitiveType.Cube);
-	planoZ.name = "PlanoZ";
-	planoZ.transform.localScale.x = 6;
-	planoZ.transform.localScale.y = 8;
-	planoZ.transform.localScale.z = 0;
-	planoZ.transform.position.x = 0.5;
-	planoZ.transform.position.y = 0;
-	planoZ.transform.position.z = -0.1;
-	planoZ.renderer.material.shader = Shader.Find("Transparent/Diffuse");
-	planoZ.renderer.material.color = Color(0,1,0,0.2);
-	planoZ.renderer.enabled=true;
+//	planoZ = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//	planoZ.name = "PlanoZ";
+//	planoZ.transform.localScale.x = 6;
+//	planoZ.transform.localScale.y = 8;
+//	planoZ.transform.localScale.z = 0;
+//	planoZ.transform.position.x = 0.5;
+//	planoZ.transform.position.y = 0;
+//	planoZ.transform.position.z = -0.1;
+//	planoZ.renderer.material.shader = Shader.Find("Transparent/Diffuse");
+//	planoZ.renderer.material.color = Color(0,1,0,0.2);
+//	planoZ.renderer.enabled=true;
 	
 }
 
 function Update () {
-Debug.DrawLine (Vector3.zero, new Vector3 (1, 0, 0), Color.red);
 	if (Input.GetKeyDown("1")) {
 		camf.enabled = true;
 		camt.enabled = false;
 		camd.enabled = false;
 		cami.enabled = false;
 		camc.enabled = false;
-		planoX.renderer.enabled=false;
-		planoY.renderer.enabled=false;
-		planoZ.renderer.enabled=true;
+//		planoX.renderer.enabled=false;
+//		planoY.renderer.enabled=false;
+//		planoZ.renderer.enabled=true;
 	}
 
 	if (Input.GetKeyDown("2")) {
@@ -73,9 +72,9 @@ Debug.DrawLine (Vector3.zero, new Vector3 (1, 0, 0), Color.red);
 		camd.enabled = false;
 		cami.enabled = false;
 	    camc.enabled = false;
-	   	planoX.renderer.enabled=false;
-		planoY.renderer.enabled=false;
-		planoZ.renderer.enabled=true;
+//	   	planoX.renderer.enabled=false;
+//		planoY.renderer.enabled=false;
+//		planoZ.renderer.enabled=true;
 	}
 
 	if (Input.GetKeyDown("3")) {
@@ -84,9 +83,9 @@ Debug.DrawLine (Vector3.zero, new Vector3 (1, 0, 0), Color.red);
 		camd.enabled = true;
 		cami.enabled = false;
 		camc.enabled = false;
-		planoX.renderer.enabled=true;
-		planoY.renderer.enabled=false;
-		planoZ.renderer.enabled=false;
+//		planoX.renderer.enabled=true;
+//		planoY.renderer.enabled=false;
+//		planoZ.renderer.enabled=false;
 	}
 
 	if (Input.GetKeyDown("4")) {
@@ -95,9 +94,9 @@ Debug.DrawLine (Vector3.zero, new Vector3 (1, 0, 0), Color.red);
 		camd.enabled = false;
 		cami.enabled = true;
 		camc.enabled = false;
-		planoX.renderer.enabled=true;
-		planoY.renderer.enabled=false;
-		planoZ.renderer.enabled=false;
+//		planoX.renderer.enabled=true;
+//		planoY.renderer.enabled=false;
+//		planoZ.renderer.enabled=false;
 	}
 	
 	if (Input.GetKeyDown("5")) {
@@ -106,8 +105,8 @@ Debug.DrawLine (Vector3.zero, new Vector3 (1, 0, 0), Color.red);
 		camd.enabled = false;
 		cami.enabled = false;
 		camc.enabled = true;
-		planoX.renderer.enabled=false;
-		planoY.renderer.enabled=true;
-		planoZ.renderer.enabled=false;
+//		planoX.renderer.enabled=false;
+//		planoY.renderer.enabled=true;
+//		planoZ.renderer.enabled=false;
 	}
 }
