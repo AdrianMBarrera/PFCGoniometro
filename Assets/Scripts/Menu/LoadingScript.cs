2d8c7315a0579051297b5a244eb189d4bb20a589
jpalazon <jospalazon@gmail.com>
2014-10-21 20:59:55 +0100

    2do intento

---
+++ Assets/Scripts/Menu/LoadingScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingScript : MonoBehaviour {
+	
+	private Text t; //texto loading
+	private Canvas LS; //LoadingScreen
+	private float delay = 0f;
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Text>();
+		t.text = "Loading";
+		LS = GameObject.Find("LoadingScreen").GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		delay += 0.35f;
+		StartCoroutine(LoadingCoroutine(delay));
+		StartCoroutine("EndLoadingCoroutine");
+
+	
+
+	}
+
+	IEnumerator LoadingCoroutine (float delay){
+		yield return new WaitForSeconds(delay);
+		if (t.text == "Loading...")
+			t.text ="Loading";
+		else
+			t.text += ".";
+	}
+
+	IEnumerator EndLoadingCoroutine(){
+		yield return new WaitForSeconds(3f);
+		LS.enabled = false;
+	}
+
+}
